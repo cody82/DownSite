a198174c416193b96789d503b3b26e72fe2bc33a
cody <sebastian.kotulla@gmx.de>
2014-11-17 19:33:03 +0100

    RSS/Atom feed

--- WebTest/Program.cs
+++ WebTest/Program.cs
@@ -21,10 +21,45 @@ using System.Diagnostics;
 using System.Security.Cryptography;
 using ServiceStack.Razor;
 using ServiceStack.Logging;
+using System.ServiceModel.Syndication;
 
 
 namespace WebTest
 {
+    [Route("/feed/{FeedName}")]
+    public class FeedRequest
+    {
+        public string FeedName { get; set; }
+    }
+
+    public class Feed : Service
+    {
+        public object Get(FeedRequest request)
+        {
+            string site = "http://komputer:1337";
+            string title = "WebTest Feed";
+            string description = title;
+            string id = "WebTestID";
+            SyndicationFeed feed = new SyndicationFeed(title, description, new Uri(site), id, DateTime.Now);
+            List<SyndicationItem> items = new List<SyndicationItem>();
+
+            var blog = PersonRepository.db.Select<Article>(x => x.ShowInBlog).OrderByDescending(x => x.Created);
+            foreach (var a in blog)
+            {
+                SyndicationItem item = new SyndicationItem(a.Title, a.Content, new Uri(site + a.Link), a.Id.ToString(), a.Created);
+                items.Add(item);
+            }
+
+            feed.Items = items;
+            Atom10FeedFormatter atomFormatter = new Atom10FeedFormatter(feed);
+            StringBuilder builder = new StringBuilder();
+            using(var writer = System.Xml.XmlWriter.Create(builder))
+                atomFormatter.WriteTo(writer);
+
+            return new HttpResult(builder.ToString(), "application/atom+xml");
+        }
+    }
+
     public class Menu
     {
         [PrimaryKey]
