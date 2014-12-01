cc7ce7505a01b82b6722a4566c678e166968a6d1
cody <sebastian.kotulla@gmx.de>
2014-12-01 19:58:22 +0100

    Fix

--- DownSite/Properties/AssemblyInfo.cs
+++ DownSite/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("FitnessService")]
+[assembly: AssemblyTitle("DownSite")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FitnessService")]
+[assembly: AssemblyProduct("DownSite")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
